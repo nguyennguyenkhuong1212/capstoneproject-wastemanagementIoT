87afb9058e2685aff53880f39d20bf042600ec46
Di-Doan <92972404+Di-Doan@users.noreply.github.com>
2024-05-06 22:35:21 +0700

    update main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import Card from "./Components/Card";
-import ProgressBar from "react-bootstrap/ProgressBar";
+import MapPage from "./Route/MapPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Card
-              name="Tu Du Hospital"
-              address="248 Cong Quynh St., Pham Ngu Lao Ward, District 1 "
-              trashPercentage={80}
-            />
-          }
-        />
+        <Route path="/" element={<MapPage />} />
       </Routes>
     </BrowserRouter>
   );
