a8cff3c42bcf52247be3d10ca5176a41ae73ccf3
Nguyễn Nguyên Khương <92970893+nguyennguyenkhuong1212@users.noreply.github.com>
2024-05-14 15:43:12 +0700

    add map

--- frontend/src/Route/MapPage.jsx
+++ frontend/src/Route/MapPage.jsx
@@ -1,12 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Card from '../Components/Card';
 import './MapPage.css';
 import pic from './holder.png';
 import Navbar from '../Components/Navbar';
 import Footer from '../Components/Footer';
 import Schedule from './Schedule';
+import "leaflet/dist/leaflet.css"
+import { MapContainer, TileLayer, Marker, Popup, useMap} from "react-leaflet"
+
+const APIkey = "5f704fb8aaca4ffca303820700c9781f"
+const ZOOM_LEVEL = 13
 
 function MapPage() {
+    const map = useRef();
+    // get user location
+    const [location, setLocation] = useState({ lat: 10.7226964, lng: 33.7055181 });
+
+    const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+    };
+
+    const success = (pos) => {
+        const crd = { lat: pos.coords.latitude, lng: pos.coords.longitude };
+        console.log(crd);
+        setLocation(crd);
+    }
+    
+    const errors = (err) => {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    useEffect(() => {
+        if (navigator.geolocation) {
+          navigator.permissions
+            .query({ name: "geolocation" })
+            .then(function (result) {
+              console.log(result);
+              if (result.state === "granted") {
+                //If granted then you can directly call your function here
+                navigator.geolocation.getCurrentPosition(success, errors, options);
+              } else if (result.state === "prompt") {
+                //If prompt then the user will be asked to give permission
+                navigator.geolocation.getCurrentPosition(success, errors, options);
+              } else if (result.state === "denied") {
+                //If denied then you have to show instructions to enable location
+              }
+            });
+        } else {
+          console.log("Geolocation is not supported by this browser.");
+        }
+      }, []);
+
+    const FlyToLocation = ({ location, zoom }) => {
+        const map = useMap();
+    
+        useEffect(() => {
+            if (map) {
+                map.flyTo(location, zoom);
+            }
+        }, [location, zoom, map]);
+    
+        return null;
+    };
+
     // Example bins data
     const binsData = [
         {
@@ -43,7 +101,15 @@ function MapPage() {
                 <div className="section">
                     <div className="title">Route Map</div>
                     <div className="container">
-                        <img className="img" src={pic} alt="Route Map" />
+                        <div className="map">
+                            <MapContainer center={location} zoom={ZOOM_LEVEL} scrollWheelZoom={false} ref={map}>
+                                <TileLayer
+                                    noWrap={false}
+                                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                                />
+                                <FlyToLocation location={location} zoom={ZOOM_LEVEL} />
+                            </MapContainer>
+                        </div>
                     </div>
                 </div>
 
