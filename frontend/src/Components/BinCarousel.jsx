a0144f42e307fa8ace9ef53fa63458ca972a149b
Di-Doan <92972404+Di-Doan@users.noreply.github.com>
2024-06-05 22:55:12 +0700

    update carousel on map page

---
+++ frontend/src/Components/BinCarousel.jsx
@@ -0,0 +1,57 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import React from "react";
+import Slider from "react-slick";
+import Card from "./Card";
+
+const BinCarousel = ({ readyToCollectBins }) => {
+
+    console.log(readyToCollectBins)
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, // Show 3 bins at a time
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  if (!readyToCollectBins || readyToCollectBins.length === 0) {
+    return <div>No bins available to display</div>;
+  }
+
+  return (
+    <div className="container">
+      <Slider {...settings}>
+        {readyToCollectBins.map((bin) => (
+          <div className="column" key={bin.name}>
+            <Card
+              name={bin.name}
+              address={bin.address}
+              trashPercentage={bin.fullness}
+            />
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default BinCarousel;
