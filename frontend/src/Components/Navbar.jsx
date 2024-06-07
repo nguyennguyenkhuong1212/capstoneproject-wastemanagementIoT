38a1c6d7a0b066ba98edb9615ac73d2f433d57e7
Di-Doan <92972404+Di-Doan@users.noreply.github.com>
2024-06-07 10:54:28 +0700

    update nav bar

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, NavLink } from "react-router-dom"; // Importing NavLink if using React Router
 import "./nav.css";
 
-function Navbar({ name }) {
+function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  // Close the mobile menu if the screen is resized to be wider than 768px
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setIsMobileMenuOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <nav className="navbar">
       <div className="second">
@@ -11,20 +29,17 @@ function Navbar({ name }) {
             <h4>WASTE MANAGEMENT IOT</h4>
           </NavLink>
         </div>
+        <div className="mobile-menu-button" onClick={toggleMobileMenu}>
+          <div className="menu-icon"></div>
+          <div className="menu-icon"></div>
+          <div className="menu-icon"></div>
+        </div>
         {/* Navigation links */}
-        <div className="nav-links">
-          <NavLink
-            to="/"
-            className="nav-link"
-            activeClassName="active-link"
-          >
+        <div className={`nav-links ${isMobileMenuOpen ? "mobile-open" : ""}`}>
+          <NavLink to="/" className="nav-link" activeClassName="active-link">
             HOME
           </NavLink>
-          <NavLink
-            to="/map"
-            className="nav-link"
-            activeClassName="active-link"
-          >
+          <NavLink to="/map" className="nav-link" activeClassName="active-link">
             MAP
           </NavLink>
           <NavLink
