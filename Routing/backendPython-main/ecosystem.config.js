391050a448dc87d24c4bab7ab0ef86254222baab
Nguyễn Nguyên Khương <92970893+nguyennguyenkhuong1212@users.noreply.github.com>
2024-06-11 16:49:34 +0700

    change ecosystem config

--- Routing/backendPython-main/ecosystem.config.js
+++ Routing/backendPython-main/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
         name: 'fastapi-app',
         script: 'uvicorn',
-        args: 'main:app --host 0.0.0.0 --port 8000',
+        args: 'main:app --host 54.251.168.188 --port 8000',
         interpreter: 'python3',
         watch: true
       }
