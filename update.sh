fdf43612574cab63259ff95c1e8699d384ecd3ff
Nguyễn Nguyên Khương <92970893+nguyennguyenkhuong1212@users.noreply.github.com>
2024-07-25 11:08:25 +0700

    "Updated update.sh script to change directory, stop pm2 process, and rebuild frontend"

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
+cd /home/ec2-user/capstoneproject-wastemanagementIoT
 git switch main
 git fetch -p
 git pull
-cd frontend
+cd /home/ec2-user/capstoneproject-wastemanagementIoT/frontend
+pm2 stop 0
 npm run build-prod
 pm2 serve ./build 3000 --name "client" --spa
